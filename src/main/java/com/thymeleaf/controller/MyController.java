78c279e203ec0259a9c380926b16a3719f8c520e
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-13 18:03:40 +0500

    Initial code

---
+++ src/main/java/com/thymeleaf/controller/MyController.java
@@ -0,0 +1,36 @@
+package com.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/about")
+    public String about(Model model){
+
+        model.addAttribute("name","Ali Raza");
+        model.addAttribute("currentDate", new Date());
+
+        System.out.println("Inside about handler");
+        return "about";
+    }
+
+
+
+    @GetMapping("/list")
+    public String nameOfList(Model m){
+
+        List<String> names = List.of("Yaseen","Kamran","Sohail","Zahid","Abrar");
+        m.addAttribute("names",names);
+
+
+
+        return "iterate";
+    }
+
+}
